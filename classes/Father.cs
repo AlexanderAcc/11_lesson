0d527ca9fb057e6f25b7283ea9171a72434d16ed
AlexanderAcc <124236169+AlexanderAcc@users.noreply.github.com>
2023-02-02 20:48:14 +0700

    Add files via upload

---
+++ classes/Father.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    internal class Father : Grandfather // наследование методов деда
+    {
+        public string father_say() // метод отца
+        {
+            return "I'm father method";
+        }
+    }
+}
---
+++ classes/Grandfather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    internal class Grandfather
+    {
+        public string grandfather_say() // Метод деда
+        {
+            return "I'm grandfather method";
+        }
+    }
+}
